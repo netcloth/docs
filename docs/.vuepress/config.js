5ed1e15753bf8118abdb93faf20a431b5e0ff829
iavl <z6521256@gmail.com>
2020-03-11 21:27:16 +0800

    updated readme for advanced docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -148,6 +148,7 @@ function genSidebarConfig (module, title) {
                     'keys',
                     'how-to-create-proposal',
                     'transaction',
+                    'contract',
                     'Q&A'
                 ]
             }
